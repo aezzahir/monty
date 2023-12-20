011f79b9ea9c3dc465c928c2cca0d0af7bf72592
aezzahir <aero.glowsky@gmail.com>
2023-12-20 15:30:27 +0100

    starting the push swap project

--- instructions.c
+++ instructions.c
@@ -23,6 +23,7 @@ void pall(monty_stack_t **stack, unsigned int line_number) {
     
     (void)line_number;
     current = *stack;
+    printf("")
     while (current != NULL) {
         printf("%d\n", current->n);
         current = current->next;
