3936a1eb98ffa734ab0d2153493040b6bbdaa02a
aezzahir <aero.glowsky@gmail.com>
2023-12-20 13:25:14 +0100

    starting the push swap project

--- utils.c
+++ utils.c
@@ -1,20 +1,37 @@
 #include "monty.h"
 
 void parse_and_execute(char *line, monty_stack_t **stack) {
-    unsigned int n;
+    char *opcode;
+    char *num_str;
+    unsigned int n = 0;
     int i;
     instruction_t instructions[] = {
         {"push", push},
         {"pall", pall},
         {NULL, NULL}
     };
-    n = (unsigned int)atoi(line + 5);
-    for (i = 0; instructions[i].opcode != NULL; i++)
-    {
-        if (strncmp(line, instructions[i].opcode, 4) == 0)
-        {
+
+    if (line == NULL) {
+        fprintf(stderr, "Error: Null line passed to parse_and_execute\n");
+        exit(EXIT_FAILURE);
+    }
+
+    opcode = strtok(line, " \t\n");
+    if (opcode == NULL) {
+        return;
+    }
+
+    num_str = strtok(NULL, " \t\n");
+
+    if (num_str != NULL) {
+        n = (unsigned int)atoi(num_str);
+    }
+
+    for (i = 0; instructions[i].opcode != NULL; i++) {
+        if (strcmp(opcode, instructions[i].opcode) == 0) {
             instructions[i].f(stack, n);
-            break;
+            return;
         }
     }
+    exit(EXIT_FAILURE);
 }
